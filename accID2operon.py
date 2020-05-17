2c0785cc2011963b16db52dc9709bd1c549dc0d1
simonsnitz <simonsnitz@gmail.com>
2020-05-17 14:27:27 -0500

    fully implemented alias-based search. Works for ~90% of genes. Added uniprot links too

--- accID2operon.py
+++ accID2operon.py
@@ -8,6 +8,7 @@ import requests
 import json
 import time
 import pickle
+import re
 
 Entrez.email = os.getenv("EMAIL", "doelsnitz@utexas.edu")
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'} 
@@ -44,8 +45,26 @@ def getUID(NCacc, seq_start, seq_stop):
         print('bad esearch request')
 
 
-def uid2MetaData(uid):
-    uid = str(uid)
+def alias2MetaData(alias):
+    
+    time.sleep(0.5)
+    response = requests.get('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=gene&term='+alias+'&retmode=json', headers=headers)
+    if response.ok: 
+        try:
+            data = json.loads(response.text)["esearchresult"]["idlist"]
+            uid = data[0]
+        except:
+            print("alias not found in esearch")
+            return
+            ########################################################
+
+    else:
+        print(response.status_code)
+        print("alias to uid esearch failed")
+        return
+    
+    time.sleep(0.5)
+    
     response = requests.get('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=gene&id='+uid+'&retmode=json', headers=headers)
     if response.ok: 
         data = json.loads(response.text)["result"][uid]
@@ -56,19 +75,35 @@ def uid2MetaData(uid):
         seq_start = gdata["chrstart"]
         seq_stop = gdata["chrstop"]
         strand = getStrand(seq_start, seq_stop)
-        return [description, NCacc, seq_start, seq_stop, strand, uid]
+
+            #get alias that works with uniprot
+        alias4uniprot = alias
+        re1 = re.compile(r'_RS')
+        if re1.search(alias):
+            newAliases = data["otheraliases"].split(',')
+            for i in newAliases:
+                if re1.search(i):
+                    pass
+                else:
+                    alias4uniprot = i
+
+        uniprotID = alias2UniprotID(alias4uniprot)
+        return [description, NCacc, seq_start, seq_stop, strand, uniprotID]
     else:
         print(response.status_code)
+        print("uid to meta data esummary failed")
+        return
 
 
-def getLinkID(uid):
+def uid2alias(uid):
     uid = str(uid)
 
-    response = requests.get('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi?dbfrom=gene&db=protein&id='+uid+'&retmode=json&idtype=acc', headers=headers)
+    response = requests.get('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=gene&id='+uid+'&retmode=json', headers=headers)
     if response.ok: 
-        P_ACC = json.loads(response.text)["linksets"][0]["linksetdbs"][0]["links"]
+        data = json.loads(response.text)["result"][uid]
         try:
-            P_ACC = P_ACC[0]
+            aliases = data["otheraliases"]
+            print(aliases)
         except:
             print("elink couldn't find any accession associated with input uid")
             return
@@ -76,20 +111,37 @@ def getLinkID(uid):
         print('bad elink request')
         return
     
-    print(P_ACC)
+    #pick alias from list. Try to avoid newer "_RS"-based aliases, since they don't work with the Uniprot API
+    aliases = aliases.split(',')
     
-    payload = {'from': 'P_REFSEQ_AC',
+    re1 = re.compile(r'_RS')
+    alias = ""
+    for i in aliases:
+        if re1.search(i):
+            alias = i
+    if len(alias) == 0:
+        alias = aliases[0]
+    
+    return alias
+    
+def alias2UniprotID(alias):
+
+    payload = {'from': 'GENENAME',
                 'to': 'ACC',
                 'format': 'list',
-                'query': P_ACC,
+                'query': alias,
                 }
 
     response = requests.get('https://www.uniprot.org/uploadlists/', params=payload, headers=headers)
-    if response.ok: 
-        return response.text
+    if response.ok:
+            #may have multiple UniprotIDs. in that case, take the first one.
+        try:
+            uniprotID = response.text.split('\n')[0]
+        except:
+            uniprotID = response.text[:-1]
+        return uniprotID
     else:
         response.raise_for_status()
-
 
 
 def getStrand(seq_start, seq_stop):
@@ -98,7 +150,7 @@ def getStrand(seq_start, seq_stop):
     elif seq_start > seq_stop:
         return "-"
 
-def getTU(uid, seq_start, strand):
+def getOperon(alias, seq_start, strand, shiftBy):
 
     '''
     Rules for inclusion/exclusion of genes from operon:
@@ -111,25 +163,50 @@ def getTU(uid, seq_start, strand):
         - if query gene direction converges with regulator, exclude it.
     '''
 
-    def getGene(geneStrand, direction, nextGene, geneList, UID):
-        #print('starting '+direction)
+        #find length of integer part in alias name
+    digits = 0
+    for i in reversed(alias):
+        if i.isdigit():
+            digits += 1
+        else:
+            break
+    print(digits)
+
+    def getGene(geneStrand, direction, nextGene, geneList, ALIAS, shiftBy):
+        
         while geneStrand == nextGene[4]:
-            #print(geneStrand, nextGene[4])
+            
             if direction == '+':
-                UID += 1
+                nextAlias = shiftAlias('+', ALIAS, shiftBy, digits)
             elif direction == '-':
-                UID -= 1
-            nextGene = uid2MetaData(UID)
+                nextAlias = shiftAlias('-', ALIAS, shiftBy, digits)
+                
+            nextGene = alias2MetaData(nextAlias)
+
             if geneStrand == '-' and nextGene[4] == '+' and direction == '+':
                 geneList.append(nextGene)
             elif geneStrand == nextGene[4]:
                 geneList.append(nextGene)
-            time.sleep(1)
+            print(nextAlias)
+            ALIAS = nextAlias
+            time.sleep(0.5)
     
+    def shiftAlias(direction, alias, shiftBy, digits):
+
+        if direction == '+':
+            index = int(alias[-digits:])
+            index1 = str(index+shiftBy).zfill(digits)
+            nextGene = alias[:-digits]+index1
+        elif direction == '-':
+            index = int(alias[-digits:])
+            index1 = str(index-shiftBy).zfill(digits)
+            nextGene = alias[:-digits]+index1
+        return nextGene
+
     geneStrand = strand
     
-    uidDOWN = uid-1
-    downGene = uid2MetaData(uidDOWN)
+    aliasDOWN = shiftAlias('-', alias, shiftBy, digits)
+    downGene = alias2MetaData(aliasDOWN)
     if seq_start > downGene[2]:
         #print('downGene is behind')
         if strand == '+' and downGene[4] == '-':
@@ -138,16 +215,16 @@ def getTU(uid, seq_start, strand):
         pass
         #print('downGene is in front')
     downgenes = [downGene]
-    getGene(geneStrand,'-',downGene, downgenes, uidDOWN)
+    getGene(geneStrand,'-',downGene, downgenes, aliasDOWN, shiftBy)
     
     geneArray = list(reversed(downgenes))
-    geneArray.append(uid2MetaData(uid))
+    geneArray.append(alias2MetaData(alias))
     regulatorIndex = (len(geneArray)-1)
 
     geneStrand = strand
-
-    uidUP = uid+1
-    upGene = uid2MetaData(uidUP)
+    
+    aliasUP = shiftAlias('+', alias, shiftBy, digits)
+    upGene = alias2MetaData(aliasUP)
     if seq_start > upGene[2]:
         #print('upGene is behind')
         if strand == '+' and upGene[4] == '-':
@@ -155,49 +232,71 @@ def getTU(uid, seq_start, strand):
     else:
         pass
         #print('upGene is in front')
-    #upgenes = [upGene]
     geneArray.append(upGene)
 
-    getGene(geneStrand,'+',upGene,geneArray, uidUP)
+    getGene(geneStrand, '+', upGene, geneArray, aliasUP, shiftBy)
 
     return geneArray, regulatorIndex
 
 
 
 if __name__=="__main__":
-    ramr = "WP_000113609" #good 7170
-    ttgr = "WP_014859138" #good 10681
-    hrtr = "NP_266817.1" #good 5147
-    bioq = "WP_011728885.1" #UIDs aren't neighbors
-    actr = "WP_011030045.1" #good 2933
-    camr = "WP_146114525.1" #No UID
-    acur = "WP_011336736.1" #good 5253
-    qacr = "WP_001807342.1" #UIDs aren't neighbors
-    beti = "NP_414847.3" #UIDs aren't neighbors
-    eilr = "WP_013366341.1" #UIDs aren't neighbors
-    tetr = "WP_000113282.1" #good 3586
-    bm3r1 = "WP_013083972.1" #good 4426
-    pfmr = "WP_011229253.1" #UIDs aren't neighbors
-    cgmr = "WP_011015249.1" #good 3353 
-    cmer = "WP_002857627.1" #UIDs aren't neighbors
-    sco7222 = "NP_631278.1" #good 6564
-    eca1819 = "WP_011093392.1" #good 3363
+    ramr = "WP_000113609" #good 7170                #alias good
+    ttgr = "WP_014859138" #good 10681               #alias good
+    hrtr = "NP_266817.1" #good 5147                 #Not working with alias - Entrez API not up to date.
+    bioq = "WP_011728885.1" #UIDs aren't neighbors  #alias good (fixed)
+    actr = "WP_011030045.1" #good 2933              #alias good (fixed)
+    camr = "WP_146114525.1" #No UID                 #still problematic
+    acur = "WP_011336736.1" #good 5253              #RS alias good
+    qacr = "WP_001807342.1" #UIDs aren't neighbors  #alias good, but something is wrong
+    beti = "NP_414847.3" #UIDs aren't neighbors     #alias good (fixed)
+    eilr = "WP_013366341.1" #UIDs aren't neighbors  #alias good (fixed)
+    tetr = "WP_000113282.1" #good 3586              #alias good
+    bm3r1 = "WP_013083972.1" #good 4426             #alias good
+    pfmr = "WP_011229253.1" #UIDs aren't neighbors  #alias good (fixed)
+    cgmr = "WP_011015249.1" #good 3353              #alias good
+    cmer = "WP_002857627.1" #UIDs aren't neighbors  #alias good (fixed)
+    sco7222 = "NP_631278.1" #good 6564              #alias good
+    eca1819 = "WP_011093392.1" #good 3363           #alias good
 
-    regName = bioq
+    regName = eilr
     Meta = acc2MetaData(regName)
     #Meta = ['NC_003197.2', '638149', '638730', '-']
-    #print(Meta)
     UID = getUID(Meta[0],Meta[1],Meta[2])
     print(UID)
     
-    #linkID = getLinkID(UID)
-    #print(linkID)
+    alias = uid2alias(UID)
+
+    MetaData = alias2MetaData(alias)
+
+    time.sleep(0.5)
+
+    re1 = re.compile(r'_RS')
+    if re1.search(alias):
+        print('RS alias found')
+        operon, regIndex = getOperon(alias, MetaData[2], MetaData[4], 5)
+    else:
+        print('RS alias not found')
+        operon, regIndex = getOperon(alias, MetaData[2], MetaData[4], 1)
+
+    operon[regIndex][0] = alias + " regulator"
+
+    print(operon)
     
+    if abs(MetaData[2]-operon[0][2]) > 10000:
+        print('something went wrong')
+    else:
+        print('everything checks out')
+
+    with open('operon.data', mode='wb') as f:
+        pickle.dump(operon, f)
+   
+
     '''
     MetaData = uid2MetaData(UID)
     
     
-    time.sleep(1)
+    time.sleep(0.5)
     TU, regIndex = getTU(UID, MetaData[2], MetaData[4])
     pprint(TU)
     #print(TU[regIndex])
