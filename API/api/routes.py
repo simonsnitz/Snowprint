19348cd90f76ce77e94930b9752c1a1f4e0bf3d6
simonsnitz <simonsnitz@penguin.lxd>
2021-11-19 07:57:21 -0600

    added clusters.html for viewing operon/info of any cluster

--- API/api/routes.py
+++ API/api/routes.py
@@ -1,9 +1,9 @@
 from flask import render_template, request, flash, redirect, url_for, jsonify
 import time
 import json
-from api import app
-#from api import db, app
-#from api.models import Sensor, Ligand, Reference
+from api import db, app
+from api.models import Cluster
+from api.render_operon_graphic import createGraphic
 import requests
 
 
@@ -13,3 +13,15 @@ def index():
     #sensorData = {s.id: {"alias":s.alias, "uniprotID":s.uniprotID, "family":s.family, "accession":s.accession} for s in sensors}
 
     return render_template('index.html', title="home page")
+
+@app.route('/cluster/<cluster_id>', methods = ['GET'])
+def cluster(cluster_id):
+    
+    cluster = Cluster.query.filter_by(id=str(cluster_id)).first_or_404()
+
+    operon = createGraphic(cluster.rep_operon, cluster.reg_index)
+
+    # clusterData = {"id":cluster.id, "EMBL":cluster.rep_EMBL, "family":cluster.family, 
+    #     "genome":cluster.rep_genome, "reg_index":cluster.reg_index}
+
+    return render_template('cluster.html', title="cluster", cluster=cluster, operon=operon)
