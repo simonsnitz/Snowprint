e7819e47783eadf358c39aa5b4769c32af11419e
Simon d'Oelsnitz <simonsnitz@gmail.com>
2022-12-17 13:06:47 -0500

    removed ugly blue background color

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ import { Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <Box 
-      sx={{backgroundColor:"#edf4ff"}}>
+      sx={{backgroundColor:"white"}}>
 
         <Welcome/>
 
